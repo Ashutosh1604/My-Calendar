b0a0070d05fb93fee8735b870b817d90f1fb324e
Ashutosh_Dhyani <82019070+Ashutosh1604@users.noreply.github.com>
2023-02-06 00:06:48 +0700

    Update ListOfEvents.js

--- MyCalendar/src/ListOfEvents.js
+++ MyCalendar/src/ListOfEvents.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import EventItem from './EventItem';
 
+import { useAuth0 } from "@auth0/auth0-react";
 
 
 function ListOfEvents(props) {
+  const { isAuthenticated } = useAuth0();
   return (
  <div className="flex flex-col w-full justify-center items-center md:p-20 py-12 px-4 gradient-bg mb-10  ">
-<h1 className="text-3xl md:text-5xl text-white ">List Of Events</h1>
+{ isAuthenticated?(<h1 className="text-3xl md:text-5xl text-white ">List Of Events</h1>):(<h1 className="text-3xl md:text-5xl text-white ">Login to see list of events</h1>)}
 <div className="flex flex-wrap justify-center items-center  ">
     {
         
-            props.allEvents.map((event)=>{
+        isAuthenticated &&    props.allEvents.map((event)=>{
               return <EventItem event={event} key={event.sno} onDelete={props.onDelete} />
             })
     }
